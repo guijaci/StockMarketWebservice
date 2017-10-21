76ca41ede18f595c932d6a9772df4992c4e3666f
Guilherme Jacichen <gui_jaci@yahoo.com.br>
2017-10-21 00:47:57 -0200

    Initial commit

---
+++ src/main/java/edu/utfpr/guilhermej/sd/stockmarketwebservice/StockmarketwebserviceApplication.java
@@ -0,0 +1,12 @@
+package edu.utfpr.guilhermej.sd.stockmarketwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockmarketwebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockmarketwebserviceApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/utfpr/guilhermej/sd/stockmarketwebservice/StockmarketwebserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.utfpr.guilhermej.sd.stockmarketwebservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StockmarketwebserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
